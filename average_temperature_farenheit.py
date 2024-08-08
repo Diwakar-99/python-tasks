4ea638d587b07e3a9168439059b00f1579f417b9
Diwakar-99 <diwababu1999@gmail.com>
2024-08-08 15:47:17 +0530

    Update average_temperature_farenheit.py

--- average_temperature_farenheit.py
+++ average_temperature_farenheit.py
@@ -3,8 +3,8 @@ celsius_temps = [20, 22, 25, 18, 30, 27, 23]
 
 fahrenheit_temps = []
 
-for temp in celcius_temps:
-    fahrenheit_temps += (temp * 9/5) + 32
+for temp in celsius_temps:
+    fahrenheit_temps.append((temp * 9/5) + 32)
 
 # Calculate the average temperature in Fahrenheit
 average_fahrenheit = sum(fahrenheit_temps) / len(fahrenheit_temps)
